6475bc7e7f11105233ae4e1221b99469bbd80271
muhammad.mahdi <aljufrimuhammad5@gmail.com>
2023-12-20 13:08:04 +0700

    feat: merge setup project

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,76 +1,72 @@
-import {useEffect, useState} from "react";
-import {db, auth} from '../firebase';
+import { useEffect, useState } from "react";
+import { db, auth } from "../firebase";
 import {
-    collection,
-    addDoc,
-    where,
-    serverTimestamp,
-    onSnapshot,
-    query,
-    orderBy,
+  collection,
+  addDoc,
+  where,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
 } from "firebase/firestore";
 
 function Home() {
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-    const messagesRef = collection(db, "messages");
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const messagesRef = collection(db, "messages");
 
-    useEffect(() => {
-        const queryMessages = query(
-            messagesRef,
-            orderBy("createdAt")
-        );
-        const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
-            let messages = [];
-            snapshot.forEach((doc) => {
-                messages.push({ ...doc.data(), id: doc.id });
-            });
-            console.log(messages);
-            setMessages(messages);
-        });
+  useEffect(() => {
+    const queryMessages = query(messagesRef, orderBy("createdAt"));
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
 
-        return () => unsuscribe();
-    }, []);
+    return () => unsuscribe();
+  }, []);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (newMessage === "") return;
-        await addDoc(messagesRef, {
-            text: newMessage,
-            createdAt: serverTimestamp(),
-            // user: auth.currentUser.displayName,
-        });
+    if (newMessage === "") return;
+    await addDoc(messagesRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      // user: auth.currentUser.displayName,
+    });
 
-        setNewMessage("");
-    };
+    setNewMessage("");
+  };
 
-    return (
-        <div className="chat-app">
-            <div className="header">
-            </div>
-            <div className="messages">
-                {messages.map((message) => (
-                    <div key={message.id} className="message">
-                        {/*<span className="user">{message.user}:</span> {message.text}*/}
-                        {message.text}
-                    </div>
-                ))}
-            </div>
-            <form onSubmit={handleSubmit} className="new-message-form">
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={(event) => setNewMessage(event.target.value)}
-                    className="new-message-input"
-                    placeholder="Type your message here..."
-                />
-                <button type="submit" className="send-button">
-                    Send
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="chat-app">
+      <div className="header"></div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            {/*<span className="user">{message.user}:</span> {message.text}*/}
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(event) => setNewMessage(event.target.value)}
+          className="new-message-input"
+          placeholder="Type your message here..."
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
