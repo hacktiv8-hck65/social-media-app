5843f7dbd71869296d3768917d0434d95b2c8039
Gema Kusuma <gemaakbarkusuma@gmail.com>
2023-12-20 14:30:09 +0700

    feat: setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import {db, auth} from './firebase';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {createBrowserRouter, redirect, RouterProvider} from "react-router-dom";
 import Home from "./views/Home.jsx";
+import {Auth} from "./views/Auth.jsx";
+import store from './store'
+import {Provider, useSelector} from "react-redux";
+
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
+    {
+        path: "/",
+        loader: () => {
+            if (!cookies.get("auth-token")) {
+                throw redirect('/login')
+            }
+            return null
+        },
+        element: <Home/>,
+    },
+    {
+        path: "/login",
+        loader: () => {
+            if (cookies.get("auth-token")) {
+                throw redirect('/')
+            }
+            return null
+        },
+        element: <Auth/>,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router}/>
+    </Provider>
 );
